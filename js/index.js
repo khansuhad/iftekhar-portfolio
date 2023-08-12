54a35ae163d5d55b772393b0f49c7cc27358b2d8
khansuhad <suhadahmodkhan@gmail.com>
2023-08-13 05:36:03 +0600

    darkmode on

---
+++ js/index.js
@@ -0,0 +1,13 @@
+
+function sun(){
+    const body = document.getElementById('body');
+    body.style.backgroundColor = 'white';
+    body.style.color = '#212121';
+}
+function nightMode(){
+    const body = document.getElementById('body');
+    body.style.backgroundColor = '#212121';
+    body.style.color = 'white';
+ }
+
+ 
